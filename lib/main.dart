4aaa042c94ee9d6d877a3cab5abd6e3fc15d9f7c
practy <info@practy.com.co>
2019-10-09 17:35:40 -0500

    Inicio de proyecto formvalidator

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'package:formvalidation/src/pages/home_page.dart';
+import 'package:formvalidation/src/pages/login_page.dart';
+ 
+void main() => runApp(FormValidation());
+ 
+class FormValidation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Form Validation',
+      initialRoute: 'login',
+      routes: {
+        'login': (BuildContext context) => LoginPage(),
+        'home' : (BuildContext context) => HomePage(),
+      },
+    );
+  }
+}
